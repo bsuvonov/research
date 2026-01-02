56c4da768052d4814241417cb339c2ed046a3fad
bsuvonov <b.suvonov2022@gmail.com>
2026-01-02 21:50:56 +0800

    feat: sort posts with the same date

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -2,13 +2,31 @@
 #
 # Check for changed posts
 
+require "time"
+
 Jekyll::Hooks.register :posts, :post_init do |post|
 
   commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
   if commit_num.to_i > 0
     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date unless lastmod_date.strip.empty?
+    lastmod_date = lastmod_date.strip
+    unless lastmod_date.empty?
+      lastmod_time = Time.parse(lastmod_date)
+      post.data["last_modified_at"] = lastmod_time
+
+      # Keep the original post date but use last push time to break ties.
+      post_date = post.date
+      post.date = Time.new(
+        post_date.year,
+        post_date.month,
+        post_date.day,
+        lastmod_time.hour,
+        lastmod_time.min,
+        lastmod_time.sec,
+        lastmod_time.utc_offset
+      )
+    end
   end
 
 end
